ce5dd7c1c26123faa4bb3514af29776bf765cfff
akramalsabri <akrm74ali@gmail.com>
2023-06-30 22:46:16 +0300

    adding catigory branch

--- src/app/page.js
+++ src/app/page.js
@@ -6,15 +6,7 @@ export default function Home() {
   return (
     <>
     <Head>
-   <script src="js/jquery/jquery-2.2.4.min.js"></script>
-    {/* <!-- Popper js --> */}
-    <script src="js/bootstrap/popper.min.js"></script>
-    {/* <!-- Bootstrap js --> */}
-    <script src="js/bootstrap/bootstrap.min.js"></script>
-    {/* <!-- All Plugins js --> */}
-    <script src="js/plugins/plugins.js"></script>
-    {/* <!-- Active js --> */}
-    <script src="js/active.js"></script>
+   
   
     </Head>
     <body>
